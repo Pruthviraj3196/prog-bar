b77ad26bf34ba1d4fa5698cbfaffd5a1e33410e3
pruthvi <pruthvirajbarbole55@gmail.com>
2024-03-14 23:16:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import ProgressBar from './component/ProgressBar';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 0.1);
+    }, 20);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} onComplete={() => setSuccess(true)} />
+      <span>{success ? <img src="https://img.freepik.com/premium-vector/checkmark-green-loading-complete_79145-705.jpg"></img> : "Loading..."}</span>
     </div>
   );
 }
